1a175cbc11aa6705d64c314348f80f92e685219b
Doston <dostonabdugafforov909@gmail.com>
2025-02-17 19:11:37 +0500

    proekt finished

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
 
 
 class PuplishedManager(models.Manager):
@@ -33,6 +34,14 @@ class Post(models.Model):
 
     objects = models.Manager()
     published = PuplishedManager()
+
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.publish.year,
+                                                             self.publish.month,
+                                                             self.publish.day,
+                                                             self.slug])
+
 
 
 
